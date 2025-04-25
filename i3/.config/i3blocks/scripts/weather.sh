547405e21b02c29dcd211539214edfbcbc5ff832
MikeRedfox <mikiguarda@gmail.com>
2025-04-25 14:10:29 +0200

    better icon in weather.sh for i3blocks

--- i3/.config/i3blocks/scripts/weather.sh
+++ i3/.config/i3blocks/scripts/weather.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 weather="$(curl -s 'https://wttr.in/Villaverla?format=1')"
-
-echo "<span background='#c4a7e7'>  $icon  </span><span  foreground='#e3e3e3' background='#2a273f'>  ${weather}%  </span>"
+icon="   "
+echo "<span background='#908caa'>  $icon  </span><span  foreground='#e3e3e3' background='#2a273f'>  ${weather}%  </span>"
