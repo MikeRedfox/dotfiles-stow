0f428c60027dd5173c17eaa0e23f5ccc7e8e1ff8
MikeRedfox <mikiguarda@gmail.com>
2025-08-31 18:13:18 +0200

    added lsp-servers to auto install

--- nvim_up/.config/nvim_up/init.lua
+++ nvim_up/.config/nvim_up/init.lua
@@ -607,8 +607,9 @@ require("lazy").setup({
 			local servers = {
 				-- clangd = {},
 				gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
+
+				basedpyright = {},
+				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
